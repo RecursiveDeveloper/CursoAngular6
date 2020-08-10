c605c1a6cfb5f38e4f076c210d99d579272ce0f6
RecursiveDeveloper <johanjesus133@hotmail.com>
2020-08-10 14:14:37 -0500

    modificacion

--- angular-wishlist/src/app/models/destino-viaje.model.ts
+++ angular-wishlist/src/app/models/destino-viaje.model.ts
@@ -1,7 +1,10 @@
 export class DestinoViaje{
 	private selected: boolean;
+	public servicios: string[];
 
-	constructor(public nombre:string, public imagenURL:string){ }
+	constructor(public nombre:string, public imagenURL:string){ 
+		this.servicios = ['pileta','desayuno'];
+	}
 
 	isSelected():boolean{
 		return this.selected;
